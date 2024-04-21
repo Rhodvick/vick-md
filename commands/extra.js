94047a4435731c37681df862d0243c5ef517b0b4
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-04-21 13:22:11 +0300

    Update extra.js

--- commands/extra.js
+++ commands/extra.js
@@ -1,15 +1,4 @@
-/*
- *
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : BLADE-MD
- * @author : SalmaytOfficial <https://github.com/Bladeh4x>
- * @description : BLADE , A Multi-functional whatsapp bot.
- * @version 0.0.6
- *
- */
+
 
 const {cmd} = require('../lib')
 const PastebinAPI = require("pastebin-js");
