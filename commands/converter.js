2b72c0a16511f794f2c2e1643261e98f3591e5e4
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-04-21 13:23:07 +0300

    Update converter.js

--- commands/converter.js
+++ commands/converter.js
@@ -1,13 +1,4 @@
-/**
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : XLICON-MD
- * @author : salmanytofficial <https://github.com/salmanytofficial/XLICON-MD>
- * @description : XLICON,A Multi-functional whatsapp bot.
- * @version 0.0.6
- **/
+
 
 const axios = require('axios')
 const { sck1, tiny, fancytext, listall,cmd,ffmpeg } = require('../lib/')
