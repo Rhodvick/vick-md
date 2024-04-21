0389d1f68fba34636436329dd1c3c5c155db557f
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-04-21 13:22:56 +0300

    Update deepsea.js

--- commands/deepsea.js
+++ commands/deepsea.js
@@ -1,18 +1,4 @@
-/**
-========================================================
-      
-      
-========================================================
- Copyright (C) 2024.                                                                                        
- Licensed under the  GPL-3.0 License;                                                      
- You may not use this file except in compliance with the License.    
- It is supplied in the hope that it may be useful                                     
- * @project_name : BLADE-MD                                                                    
- * @author : naveeddogar <https://github.com/bladeh4x>   
- * @description : BLADE-MD ,A Multi-functional whatsapp bot.       
- * @version 1.0.1                                                                                             
- ========================================================
- **/
+
 
  const { tlang,cmd,Config } = require('../lib')
  const maker = require('mumaker')
